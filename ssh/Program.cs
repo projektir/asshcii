69c848f30994c6680f3998f9886b4b463a864609
victor.koenders <victor.koenders@gmail.com>
2017-12-08 16:48:35 +0100

    Initial framework for the ssh server

---
+++ ssh/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ssh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class ClientState
+    {
+        public IPAddress IpAddress { get; set; }
+    }
+
+    public class SshServer<TClientState> where TClientState : ClientState, new()
+    {
+        public List<Client<TClientState>> Clients { get; set; }
+        public delegate void ClientConnected(SshServer<TClientState> sender, TClientState state);
+        public delegate void ClientDisconnected(SshServer<TClientState> sender, TClientState state);
+        public delegate void ClientMessage(SshServer<TClientState> sender, TClientState state);
+    }
+
+    public class Client<TClientState> where TClientState : ClientState
+    {
+        public TClientState State { get; set; }
+        public TcpClient Socket { get; set; }
+    }
+}
